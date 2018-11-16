ad01bbad7c337ff5b31677448dd454a3338e1326
Junrui <raymond.jr.lin@gmail.com>
2018-11-16 16:57:39 -0500

    update

--- ASCIIEncodedStrings.java
+++ ASCIIEncodedStrings.java
@@ -1,62 +1,34 @@
 public class ASCIIEncodedStrings {
-    private static String decode(String input) {
-        s = s.reverse();
+    private static String decode(String s) {
+        s = new StringBuilder(s).reverse().toString();
+        // System.out.println("reversed : " + s);
         StringBuilder sb = new StringBuilder();
         int i = 0;
         while (i < s.length()) {
+            // System.out.print("now check from " + i + " to ");
             if (s.charAt(i) == '1') {
-                int num = 0;
-                for (int j = 0; j < 3; j++) {
-                    num = num * 10 + (int) (s.charAt(j));
-                }
-                sb.append((char) num);
+                sb.append((char) getNum(s, i, 3));
                 i += 3;
             } else {
-                for (int j = 0; j < 2; j++) {
-                    num = num * 10 + (int) (s.charAt(j));
-                }
-                sb.append((char) num);
+                sb.append((char) getNum(s, i, 2));
                 i += 2;
             }
         }
         return sb.toString();
     }
-
-    // test
-    private static String decode(String s) {
-        s = new StringBuilder(s).reverse().toString();
-        System.out.println("reversed : " + s);
-        StringBuilder sb = new StringBuilder();
-        int i = 0;
-        while (i < s.length()) {
-            System.out.print("now check from " + i + " to ");
-            if (s.charAt(i) == '1') {
-                System.out.print(i + 3 + " , ");
-                int num = 0;
-                for (int j = i; j < 3; j++) {
-                    System.out.print((s.charAt(j) - '0') + ", ");
-                    num = num * 10 + (s.charAt(j) - '0');
-                }
-                System.out.println(", get number: " + num + ", its char is " + (char)num);
-                sb.append((char) num);
-                i += 3;
-            } else {
-                System.out.println(i + 2);
-                int num = 0;
-                for (int j = i; j < 2; j++) {
-                    System.out.print((s.charAt(j) - '0') + ", ");
-                    num = num * 10 + (s.charAt(j) - '0');
-                }
-                System.out.println(", get number: " + num + ", its char is " + (char)num);
-                sb.append((char) num);
-                i += 2;
-            }
+    private static int getNum(String s, int start, int steps) {
+        int num = 0;
+        // System.out.print(start + count + " : ");
+        for (int i = 0; i < steps; i++) {
+            // System.out.print((s.charAt(i + i) - '0') + ", ");
+            num = num * 10 + (s.charAt(start + i) - '0');
         }
-        return sb.toString();
+        // System.out.println(", get number: " + num + ", its char is " + (char)num);
+        return num;
     }
 
     public static void main(String[] args) {
-        String s = "729799107101114328297110107";
+        String s = "701011792823411101701997927";
         System.out.println(decode(s));
     }
 }
